88f834413cdca243826712959f4a88e33a1aa163
Dzianis <tdenis.20012510@gmail.com>
2022-04-07 13:12:20 +0300

    fixed lab06

--- lab06/app/src/main/java/com/example/lab06/MainActivity.java
+++ lab06/app/src/main/java/com/example/lab06/MainActivity.java
@@ -1,38 +1,57 @@
 package com.example.lab06;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button dialog;
-    private Button swipe;
-
+public class MainActivity extends ListActivity {
+    private final String[] list = { "Dialog Activity",
+            "Tabed Activity",
+            "Another 1" ,
+            "Another 2",
+            "Another 3",
+            "Another 4",
+            "Another 5",
+            "Another 6",
+            "Another 7",
+            "Another 8",
+            "Another 9",
+            "Another 10",
+            "Another 11",
+            "Another 12",
+            "Another 13"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        dialog = findViewById(R.id.button);
-        swipe = findViewById(R.id.button2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        setListAdapter(adapter);
 
-        dialog.setOnClickListener(new View.OnClickListener() {
+        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), DialogActivity.class);
-                startActivity(intent);
-            }
-        });
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                if(position == 0){
+                    Intent intent = new Intent(v.getContext(), DialogActivity.class);
+                    startActivity(intent);
+                }
 
-        swipe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), TabedActivity.class);
-                startActivity(intent);
+                if(position == 1){
+                    Intent intent1 = new Intent(v.getContext(), TabedActivity.class);
+                    startActivity(intent1);
+                }
+
+                if(position >= 2){
+                    Toast.makeText(getApplicationContext(),
+                            parent.getItemAtPosition(position).toString(),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
             }
-        });
+        };
+        getListView().setOnItemClickListener(itemListener);
     }
 }
