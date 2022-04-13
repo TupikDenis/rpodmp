a4d6ac9c0f835f027549ae296573c59ebf983507
Dzianis <tdenis.20012510@gmail.com>
2022-04-13 23:35:26 +0300

    add lab10

---
+++ lab10/app/src/main/java/com/example/lab10/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.lab10;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends Activity {
+
+    private static final int PERMISSION_REQUEST = 1;
+    private TextView tvEnabledGPS;
+    private TextView tvStatusGPS;
+    private TextView tvLocationGPS;
+
+    private LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvEnabledGPS = (TextView) findViewById(R.id.tvEnabledGPS);
+        tvStatusGPS = (TextView) findViewById(R.id.tvStatusGPS);
+        tvLocationGPS = (TextView) findViewById(R.id.tvLocationGPS);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000 * 10, 10, locationListener);
+        checkEnabled();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            checkEnabled();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            checkEnabled();
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST);
+            }
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                tvStatusGPS.setText("Status: " + String.valueOf(status));
+            }
+        }
+    };
+
+    private void showLocation(Location location) {
+        if (location == null)
+            return;
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            tvLocationGPS.setText(formatLocation(location));
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    private String formatLocation(Location location) {
+        if (location == null)
+            return "";
+        return String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f",
+                location.getLatitude(), location.getLongitude());
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void checkEnabled() {
+        tvEnabledGPS.setText("Enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER));
+    }
+
+    public void onClickLocationSettings(View view) {
+        startActivity(new Intent(
+                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+    };
+
+}
+
